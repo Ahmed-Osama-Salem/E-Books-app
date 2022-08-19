fff1f39b5e1153d5ee303713e02baa513e661c89
Ahmed-Osama-Salem <97908950+Ahmed-Osama-Salem@users.noreply.github.com>
2022-08-20 00:10:17 +0200

    full final

--- src/compoments/Footer.jsx
+++ src/compoments/Footer.jsx
@@ -7,7 +7,11 @@ function Footer() {
       <div class="container mx-auto px-8 ">
         <div class="w-full flex flex-col md:flex-row py-6 ">
           <div class=" mb-6 text-black">
-            <img src="images/book-logo.png" alt="book" className="w-10 flex" />
+            <img
+              src="images/book-logo.png"
+              alt="book"
+              className="w-10 flex imagebook"
+            />
             <a
               class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl mr-5"
               href="#"

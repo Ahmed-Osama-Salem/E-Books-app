f437b0c386518a8f89f8662ba3d72acc31e71ce1
Ahmed-Osama-Salem <97908950+Ahmed-Osama-Salem@users.noreply.github.com>
2022-08-19 21:34:59 +0200

    last update

--- src/compoments/BookSection.jsx
+++ src/compoments/BookSection.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function BookSection() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className="book-area">
-      <h1>Books Area</h1>
-      <p>Search in E-Books the most huge library.</p>
+      <h1 data-aos="fade-down-right">Books Area</h1>
+      <p data-aos="zoom-in-up">Search in E-Books the most huge library.</p>
     </div>
   );
 }
